3f863397c9970a0c2722a0244454e6f5a096b639
naveenpairala <106140429+naveenpairala@users.noreply.github.com>
2022-06-30 16:06:23 +0530

    Add files via upload

---
+++ DisplayDay.java
@@ -0,0 +1,66 @@
+/*
+  JFM1T4_Assignment2:
+  Write a program to display the name of the day, based on the number, using the switch statement.
+  Prompt the user to input a number between 1 and 7. 
+  
+  Sample Input: 1
+  
+  Expected Output: Monday
+  
+*/
+
+//import statements for java program to read inputs using Scanner class 
+import java.util.Scanner;
+
+public class DisplayDay {
+
+//declare number variable
+  public static void main(String[] args){
+  int day;
+  
+
+//Use the scanner class to provide input at execution time using scanner object
+//Scanner s= new Scanner(System.in)
+  Scanner sc=new Scanner(System.in);
+  day=sc.nextInt();
+    System.out.println("display day");
+
+
+/*
+Take input of the number for displaying day
+System.out.println("Enter value for displaying day:");
+number=sc.nextInt();
+*/
+
+//Declare switch case and check the input value to print the day entered by the user
+    switch (day){
+      case 1:
+      System.out.println("dispaly the monday");
+      break;
+      case 2:
+      System.out.println("display the thuday");
+      break;
+      case 3:
+      System.out.println("display the wenday");
+       break;
+      case 4:
+        System.out.println("display the thruday");
+      break;
+      case 5:
+        System.out.println("display the friday");
+        break;
+      case 6:
+        System.out.println("display the satday");
+        break;
+      case 7:
+        System.out.println("display the sunday");
+    }
+  }
+
+
+
+        
+
+
+    }
+     
---
+++ HighestNumber.java
@@ -0,0 +1,60 @@
+/* JFM1T4_Assignment1:
+   Write a program to find the highest among the given 3 numbers.
+   Prompt the user for the 3 numbers to be input from the terminal.
+
+   SampleInput: 
+   Enter the first number: 11
+   Enter the second number: 22
+   Enter the third number: 1
+
+   Expected Output:
+   22 is highest number
+ 	
+*/
+
+//import statements for java program to read inputs using Scanner class 
+import java.util.*;
+
+public class HighestNumber {
+     
+//Add main method
+  public static void main(String[] args){
+    
+  
+
+//Declare the three variables
+  int n,m,a;
+
+//Use the scanner class to provide input at execution time
+  
+/* 
+ Scanner s= new Scanner(System.in);
+ System.out.println("Enter the first number");
+ number1=s.nextInt();
+ Scanner sc = new Scanner(System.in);
+
+*/
+  Scanner sc = new Scanner(System.in);
+  System.out.println("enter the first number");
+   n=sc.nextInt();
+   System.out.println("enter the second number");
+    m=sc.nextInt();
+    System.out.println("enter the thrid number");
+    a=sc.nextInt();
+    //check which number is highest using if else condition
+if(n>m&&n>a){
+  
+
+    System.out.println("highest number is ="+n);
+}
+    else if(m>n&&m>a){
+      System.out.println("heighest number is ="+m);
+      
+    }
+    else if(a>m&&a>n){
+      System.out.println("heighest number is="+n);
+    }
+
+//Print the highest of three numbers
+  }
+}
